8ffe4f35ab8b4c1648f8ed10804534a86d488a10
Abel-Bonicel <122082314+Abel-Bonicel@users.noreply.github.com>
2023-12-20 13:58:11 +0100

    Add files via upload
    Mise en ligne des fichiers du projet qui fonctionne mais qui peuvent être améliorer

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from flask import Flask, render_template, request
+import search
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST' :
+        request_search = request.form['search_product']
+        return render_template('product.html', search_product = search.search_Product(request_search))
+    else:
+        return render_template('index.html')
---
+++ search.py
@@ -0,0 +1,47 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+#Définition de recherche de produit
+def search_Product(req):
+    #Initialisation du navigateur Chrome et de son Driver
+    service = Service(executable_path='chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+
+    form_req = req.replace(' ', '+')
+    driver.get(f'https://www.google.fr/search?q={form_req}&source=lnms&tbm=shop')
+    sleep(2)
+    driver.find_element('xpath', "//span[text()='Tout refuser']").click()
+
+    sleep(2)
+    global list_product
+    list_product = []
+    for product in driver.find_elements(By.CLASS_NAME, 'i0X6df')[:20]:
+        result_image = product.find_element(By.TAG_NAME, 'img')
+        try:
+            image = result_image.get_attribute('src')
+        except:
+            image = "-"
+        description = product.find_element(By.CLASS_NAME, 'tAxDx')
+        prix = product.find_element(By.CLASS_NAME, 'a8Pemb')
+        seller = product.find_element(By.CLASS_NAME, 'aULzUe')
+        livraison = product.find_element(By.CLASS_NAME, 'vEjMR')
+        result_link = product.find_element(By.TAG_NAME, 'a')
+        try: 
+            link = result_link.get_attribute('href')
+        except:
+            link = '-'
+        list_product.append({"image": image, "description": description.text, "prix": prix.text.replace('\u202f', ''), "seller": seller.text, "livraison": livraison.text, "link":link})
+    driver.close()
+    return sort_Product(list_product)
+
+
+#Fonction de tri de résultats
+def sort_Product(list_product):
+    global sorted_list
+    sorted_list = sorted(list_product, key=lambda x: float(x["prix"].replace('€', '').replace(',', '.').replace('\u202f', '').replace('&nbsp;', '')))
+    return sorted_list
+
