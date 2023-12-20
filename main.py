abf76d343530962495ad9cb6715078c210d0cee8
Rhomancode <102428478+Rhomancode@users.noreply.github.com>
2023-12-20 22:46:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST' :
-        request_search = request.form['search_product']
-        return render_template('product.html', search_product = search.search_Product(request_search))
+        if request.form['search_product'] != '':
+            request_search = request.form['search_product']
+            return render_template('product.html', search_product = search.search_Product(request_search))
+        else:
+                return render_template('index.html')
     else:
         return render_template('index.html')
